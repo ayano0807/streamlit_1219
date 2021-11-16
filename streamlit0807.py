fdd585f199b451838aecf2931a885915655656c6
ayano0807 <93708382+ayano0807@users.noreply.github.com>
2021-11-16 15:48:06 +0900

    Update streamlit0807.py

--- streamlit0807.py
+++ streamlit0807.py
@@ -6,9 +6,12 @@ Created on Thu Nov 11 17:14:15 2021
 """
 import streamlit as st
 
-st.title ('ジャニーズってすごい！！')
+st.title ('ジャニーズしか勝たん！！')
 st.write ('そこの君、ジャニーズの凄さを知らないな？')
 st.write('そんな君にジャニーズの凄さを教えようではないか。')
+
+import pandas as pd
+
 
 
 
