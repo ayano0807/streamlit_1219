bda76d65799049aad04cd9651d3e84e32f1b8e34
ayano0807 <20e1806h@shinshu-u.ac.jp>
2021-12-01 09:40:48 +0900

    4

--- streamlit0807.py
+++ streamlit0807.py
@@ -11,8 +11,24 @@ st.write ('そこの君、ジャニーズの凄さを知らないな？')
 st.write('そんな君にジャニーズの凄さを教えようではないか。')
 
 import pandas as pd
+<<<<<<< Updated upstream
 
 
+=======
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+
+#C:/Users/ayano/Desktop/python/Arashi.py.csv
+data = pd.read_csv("Arashi.py")
+
+df.plot()
+plt.show()
+
+print("ジャニーズは嵐だけではない！他にも多くのアーティストが存在し人々に影響を与えているのだ！")
+print("そんなあなたにおすすめのグループは…？")
+
+print()
+>>>>>>> Stashed changes
 
 
 
